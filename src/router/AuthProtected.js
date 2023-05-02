2595a94eb14976f9b49156f99af17565a38b609f
dncuong <dinhcuongnd2001@gmail.com>
2023-05-02 14:31:58 +0700

    .gitignore is now working

--- src/router/AuthProtected.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useProfile } from "../Components/Hooks/UserHook";
-import { Navigate } from "react-router-dom";
-function AuthProtected(props) {
-  const { userProfile, loading } = useProfile();
-  return !userProfile ? <Navigate to="/login" /> : props.children;
-}
-
-export default AuthProtected;
